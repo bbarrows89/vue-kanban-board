6fc886a3f6a3b56c29f58962ac0c929641d6c867
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-06-02 16:39:25 -0700

    added form component to add new items

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="backlog-view">
-    <p>Backlog</p>
+    <new-item></new-item>
   </div>
 </template>
 
 <script>
+import NewItemForm from './NewItemForm';
+
 export default {
   name: 'Backlog',
-}
+  components: {
+    'new-item': NewItemForm,
+  },
+};
 </script>
