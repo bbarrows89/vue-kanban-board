c5b627d8ff41d930b04621c705407bb3be441986
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-06-02 16:25:41 -0700

    added Vuex store and theme

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+    <div class="page-header">
+      <h1>Kanban Board</h1>
+      <p class="lead">An example of a Kanban board, written in Vue.js</p>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+  .page-header h1 {
+    font-weight: 300;
+  }
+</style>
