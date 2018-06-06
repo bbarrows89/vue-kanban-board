7e88238e6f15ac4b902f1b319ec2fca723b0f9b2
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-06-06 16:16:48 -0700

    modify MenuBar to bind class value for buttons based upon current path

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="menubar">
     <div class="btn-group" role="group" aria-label="Menu bar">
-      <router-link to="/board">Board</router-link>
-      <router-link to="/backlog">Backlog</router-link>
+      <router-link to="/board" :class="buttonClass('/board')">Board</router-link>
+      <router-link to="/backlog" :class="buttonClass('/backlog')">Backlog</router-link>
     </div>
   </div>
 </template>
@@ -10,6 +10,15 @@
 <script>
 export default {
   name: 'MenuBar',
+  methods: {
+    buttonClass(path) {
+      return {
+        btn: true,
+        'btn-primary': this.$route.path === path,
+        'btn-secondary': this.$route.path !== path
+      };
+    }
+  }
 }
 </script>
 
