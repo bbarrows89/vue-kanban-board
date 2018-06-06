4d1d6cbfa556d0ccf5266cddab1748e77401fa6e
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-06-06 16:12:04 -0700

    add a menu-bar component using <router-link>s

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,20 @@
       <p class="lead">An example of a Kanban board, written in Vue.js</p>
     </div>
 
+    <menu-bar></menu-bar>
+
     <router-view/>
   </div>
 </template>
 
 <script>
+import MenuBar from '@/components/MenuBar';
+
 export default {
   name: 'app',
+  components: {
+    'menu-bar': MenuBar
+  }
 };
 </script>
 
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="menubar">
+    <div class="btn-group" role="group" aria-label="Menu bar">
+      <router-link to="/board">Board</router-link>
+      <router-link to="/backlog">Backlog</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuBar',
+}
+</script>
+
+<style>
+  .menubar {
+    margin-bottom: 16px;
+  }
+</style>
